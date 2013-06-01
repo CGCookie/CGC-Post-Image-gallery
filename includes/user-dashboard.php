e1d7b2f23a2d9ac947027a184875e0d2c4584d11
Pippin Williamson <pippin@pippinsplugins.com>
2013-05-31 23:26:39 -0500

    Remove default image

--- includes/user-dashboard.php
+++ includes/user-dashboard.php
@@ -6,11 +6,11 @@ function pig_user_dashboard_images() {
 
 	// get all network sites
 	$network_sites = get_blogs_of_user(1, false);
-	
+
 	//echo '<pre>';print_r($network_sites); echo '</pre>';
 	foreach($network_sites as $site) :
 
-		if($site->path != '/') {		
+		if($site->path != '/') {
 			$image_args = array(
 				'author' => $current_user->ID,
 				'post_type' => 'images',
@@ -25,8 +25,8 @@ function pig_user_dashboard_images() {
 			// The Query
 			$the_query = new WP_Query($image_args);
 
-		echo '<div class="site-portfolio">';		
-			if($the_query->have_posts()) : 
+		echo '<div class="site-portfolio">';
+			if($the_query->have_posts()) :
 				echo '<h5 class="site-portfolio-name">' . $site->blogname . ' Images</h5>';
 				echo '<span class="site-portfolio-controls">';
 					echo '<a href="' . network_home_url($site->path . 'profile/' . $current_user->user_login) . '"><i class="icon-eye-open"></i> view gallery</a>';
@@ -39,8 +39,6 @@ function pig_user_dashboard_images() {
 						<a href="<?php echo get_post_meta(get_the_ID(), 'pig_image_url', true); ?>" title="View this image">
 							<?php if(get_the_post_thumbnail(get_the_ID(), 'medium-thumb')) { ?>
 								<?php the_post_thumbnail('medium-thumb'); ?>
-							<?php } else { ?>
-								<img class="attachment-pig-image-dashboard" src="<?php echo get_post_meta(get_the_ID(), 'pig_dashboard_image_url', true); ?>"/>
 							<?php } ?>
 						</a>
 						<ul class="gallery-image-controls">
@@ -50,17 +48,17 @@ function pig_user_dashboard_images() {
 								<div class="image-title hidden"><?php echo get_the_title(); ?></div>
 								<div class="image-description hidden"><?php echo htmlentities(get_the_content()); ?></div>
 								<div class="image-subsite-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_id', true); ?></div>
-								<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div> 
+								<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div>
 							</li>
 							<li id="remove-<?php echo get_the_ID(); ?>" class="delete-image">
 								<a href="#image-delete-modal" name="image-delete-modal" title="Delete this Image"><i class="icon-remove"></i></a>
  								<div class="image-subsite-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_id', true); ?></div>
-								<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div> 
-							</li>							
+								<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div>
+							</li>
 						</ul>
 					</div>
-				<?php 
-				endwhile; 
+				<?php
+				endwhile;
 				echo '</div><!-- ends #user-portfolio-images -->'; // ends #user-portfolio-images
 			endif;
 
@@ -70,32 +68,32 @@ function pig_user_dashboard_images() {
 				echo '<h5 class="site-portfolio-name">' . $site->blogname . ' Images</h5>';
 				echo '<span class="site-portfolio-controls">';
 					echo '<a href="'. $site->siteurl .'/gallery/submit-image" title="Submit a new image"><i class="icon-plus"></i> Add Image</a>';
-				echo '</span>';					
+				echo '</span>';
 				echo '<p class="empty">You have no images uploaded to '. $site->blogname .'</p>';
 			endif;
 
 			echo '</div><!-- ends .site-portfolio -->'; // ends .site-portfolio
-		
+
 			// Reset Post Data
 			wp_reset_postdata();
 		}
 	endforeach; // foreach sites
 
 	return ob_get_clean();
-} 
+}
 
 function pig_image_edit_form() {
 	ob_start(); ?>
-	
+
 	<form id="pig-image-edit" action="" method="POST"/>
-		
+
 		<input type="hidden" name="pig-image-action" value="edit" />
 		<input type="hidden" id="pig-image-id" name="pig-image-id" value="" />
 		<input type="hidden" id="pig-subsite-id" name="pig-subsite-id" value="" />
 		<input type="hidden" id="pig-subsite-image-id" name="pig-subsite-image-id" value="" />
 		<input type="hidden" id="pig-referrer" name="pig-referrer" value="<?php the_permalink(); ?>" />
 		<input type="text" id="pig-image-title" name="pig-image-title" value="" />
-		<label for="pig-image-title">Enter in a descriptive image title</label>		
+		<label for="pig-image-title">Enter in a descriptive image title</label>
 		<textarea id="pig-image-desc" name="pig-image-desc"></textarea>
 		<label for="pig-image-desc">What software was used, how did you make it... things inquiring minds would want to know</label>
 		<input type="checkbox" id="pig-image-mature" name="pig-image-mature" value="1" />
@@ -103,7 +101,7 @@ function pig_image_edit_form() {
 		<input type="submit" class="button" id="pig_submit" value="Submit Update" />
 		<a href="#" class="close" id="pig_cancel">Cancel</a>
 	</form>
-	
+
 	<?php
 	return ob_get_clean();
 }
@@ -114,7 +112,7 @@ function pig_image_remove_form() {
 		<h3>Are you sure you want to delete</h3>
 		<p><strong>Clicking yes will obliterate the data beyond recovery</strong></p>
 		<form id="pig-image-remove" action="" method="POST"/>
-			
+
 			<input type="hidden" name="pig-image-action" value="delete" />
 			<input type="hidden" id="pig-delete-image-id" name="pig-image-id" value="" />
 			<input type="hidden" id="pig-delete-subsite-id" name="pig-subsite-id" value="" />
