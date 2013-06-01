d7f4385dbb39c01a702bf6aaec8d0f28912aefde
Pippin Williamson <pippin@pippinsplugins.com>
2013-05-31 23:53:28 -0500

    updated dashboard

--- includes/user-dashboard.php
+++ includes/user-dashboard.php
@@ -7,78 +7,76 @@ function pig_user_dashboard_images() {
 	// get all network sites
 	$network_sites = get_blogs_of_user(1, false);
 
-	//echo '<pre>';print_r($network_sites); echo '</pre>';
-	foreach($network_sites as $site) :
+	$image_args = array(
+		'author' => $current_user->ID,
+		'post_type' => 'images',
+		'posts_per_page' => -1
+	);
 
-		if($site->path != '/') {
-			$image_args = array(
-				'author' => $current_user->ID,
-				'post_type' => 'images',
-				'posts_per_page' => -1,
-				'meta_query' => array(
-					array(
-						'key' => 'pig_subsite_id',
-						'value' => $site->userblog_id
-					)
-				)
-			);
-			// The Query
-			$the_query = new WP_Query($image_args);
+	foreach( $network_sites as $site ) :
+
+		if( $site->userblog_id == 1 )
+			continue;
+
+		switch_to_blog( $site->userblog_id );
+
+		// The Query
+		$the_query = new WP_Query($image_args);
 
 		echo '<div class="site-portfolio">';
-			if($the_query->have_posts()) :
-				echo '<h5 class="site-portfolio-name">' . $site->blogname . ' Images</h5>';
-				echo '<span class="site-portfolio-controls">';
-					echo '<a href="' . network_home_url($site->path . 'profile/' . $current_user->user_login) . '"><i class="icon-eye-open"></i> view gallery</a>';
-					echo '<a href="'. $site->siteurl .'/gallery/submit-image" title="Submit a new image"><i class="icon-plus"></i> Add Image</a>';
-				echo '</span>';
-				echo '<div id="user-portfolio-images" class="gallery clearfix">';
-				// The Loop
-				while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		if( $the_query->have_posts() ) :
+			echo '<h5 class="site-portfolio-name">' . $site->blogname . ' Images</h5>';
+			echo '<span class="site-portfolio-controls">';
+				echo '<a href="' . network_home_url($site->path . 'profile/' . $current_user->user_login) . '"><i class="icon-eye-open"></i> view gallery</a>';
+				echo '<a href="'. $site->siteurl .'/gallery/submit-image" title="Submit a new image"><i class="icon-plus"></i> Add Image</a>';
+			echo '</span>';
+			echo '<div id="user-portfolio-images" class="gallery clearfix">';
+			// The Loop
+			while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 				<div class="pig-grid-image">
-						<a href="<?php echo get_post_meta(get_the_ID(), 'pig_image_url', true); ?>" title="View this image">
-							<?php if(get_the_post_thumbnail(get_the_ID(), 'medium-thumb')) { ?>
-								<?php the_post_thumbnail('medium-thumb'); ?>
-							<?php } else { ?>
-								<img class="attachment-pig-image-dashboard" src="<?php echo get_post_meta(get_the_ID(), 'pig_dashboard_image_url', true); ?>"/>
-							<?php } ?>
-						</a>
-						<ul class="gallery-image-controls">
-							<li id="<?php echo get_the_ID(); ?>" class="edit-image">
-								<a id="image-edit-modal-toggle" href="#" data-reveal-id="image-edit-modal" title="Edit this Image"><i class="icon-pencil"></i></a>
- 								<div class="image-mature hidden"><?php if(get_post_meta(get_the_ID(), 'pig_mature', true)) { echo 'yes'; } else { echo 'no'; } ?></div>
-								<div class="image-title hidden"><?php echo get_the_title(); ?></div>
-								<div class="image-description hidden"><?php echo htmlentities(get_the_content()); ?></div>
+					<a href="<?php echo get_post_meta(get_the_ID(), 'pig_image_url', true); ?>" title="View this image">
+						<?php if( get_the_post_thumbnail( get_the_ID(), 'medium-thumb') ) { ?>
+							<?php the_post_thumbnail('medium-thumb'); ?>
+						<?php } else { ?>
+							<img class="attachment-pig-image-dashboard" src="<?php echo get_post_meta(get_the_ID(), 'pig_dashboard_image_url', true); ?>"/>
+						<?php } ?>
+					</a>
+					<ul class="gallery-image-controls">
+						<li id="<?php echo get_the_ID(); ?>" class="edit-image">
+							<a id="image-edit-modal-toggle" href="#" data-reveal-id="image-edit-modal" title="Edit this Image"><i class="icon-pencil"></i></a>
+								<div class="image-mature hidden"><?php if(get_post_meta(get_the_ID(), 'pig_mature', true)) { echo 'yes'; } else { echo 'no'; } ?></div>
+							<div class="image-title hidden"><?php echo get_the_title(); ?></div>
+							<div class="image-description hidden"><?php echo htmlentities(get_the_content()); ?></div>
+							<div class="image-subsite-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_id', true); ?></div>
+							<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div>
+						</li>
+						<li id="remove-<?php echo get_the_ID(); ?>" class="delete-image">
+							<a href="#image-delete-modal" name="image-delete-modal" title="Delete this Image"><i class="icon-remove"></i></a>
 								<div class="image-subsite-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_id', true); ?></div>
-								<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div>
-							</li>
-							<li id="remove-<?php echo get_the_ID(); ?>" class="delete-image">
-								<a href="#image-delete-modal" name="image-delete-modal" title="Delete this Image"><i class="icon-remove"></i></a>
- 								<div class="image-subsite-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_id', true); ?></div>
-								<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div>
-							</li>
-						</ul>
-					</div>
-				<?php
-				endwhile;
-				echo '</div><!-- ends #user-portfolio-images -->'; // ends #user-portfolio-images
-			endif;
-
-			?>
+							<div class="image-subsite-image-id hidden"><?php echo get_post_meta(get_the_ID(), 'pig_subsite_image_id', true); ?></div>
+						</li>
+					</ul>
+				</div>
 			<?php
-			if( !$the_query->have_posts()) :
-				echo '<h5 class="site-portfolio-name">' . $site->blogname . ' Images</h5>';
-				echo '<span class="site-portfolio-controls">';
-					echo '<a href="'. $site->siteurl .'/gallery/submit-image" title="Submit a new image"><i class="icon-plus"></i> Add Image</a>';
-				echo '</span>';
-				echo '<p class="empty">You have no images uploaded to '. $site->blogname .'</p>';
-			endif;
-
-			echo '</div><!-- ends .site-portfolio -->'; // ends .site-portfolio
+			endwhile;
+			echo '</div><!-- ends #user-portfolio-images -->'; // ends #user-portfolio-images
 
 			// Reset Post Data
 			wp_reset_postdata();
-		}
+
+		else :
+
+			echo '<h5 class="site-portfolio-name">' . $site->blogname . ' Images</h5>';
+			echo '<span class="site-portfolio-controls">';
+				echo '<a href="'. $site->siteurl .'/gallery/submit-image" title="Submit a new image"><i class="icon-plus"></i> Add Image</a>';
+			echo '</span>';
+			echo '<p class="empty">You have no images uploaded to '. $site->blogname .'</p>';
+		endif;
+
+		echo '</div><!-- ends .site-portfolio -->'; // ends .site-portfolio
+
+		restore_current_blog();
+
 	endforeach; // foreach sites
 
 	return ob_get_clean();
