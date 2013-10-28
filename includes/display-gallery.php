a9b278366e0b138f757f955ca021a9640d715f69
Brian DiChaira <briandichiara@gmail.com>
2013-10-28 12:28:12 -0500

    wrong var name

--- includes/display-gallery.php
+++ includes/display-gallery.php
@@ -37,12 +37,17 @@ function pig_display_gallery( $post_id, $display = 8 ) {
 }
 
 function pig_get_image( $size = 'full' ) {
-	$thumb   = get_post_thumbnail_id();
-	$img_url = wp_get_attachment_image_src( $thumb, $size );
+	$thumb = get_post_thumbnail_id();
+	$image = wp_get_attachment_image_src( $thumb, $size );
 	if( $image ) {
 		$src = $image[0];
-		$headers = @get_headers( $src );
-		if( strpos( $headers[0], '404' ) === false )
+		if( @ini_get( 'allow_url_fopen' ) ){
+			$exists = file_exists( $src );
+		} else {
+			$headers = @get_headers( $src );
+			$exists = (strpos( $headers[0], '404' ) === false);
+		}
+		if( $exists )
 			return $src;
 
 		$check_flag = get_post_meta( get_the_ID(), '_pig_image_404', true );
