5cfef1da815427b4d0f2b24e854ea36896003e58
Brian DiChaira <briandichiara@gmail.com>
2013-10-29 17:31:11 -0500

    admin page rendering

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -2,18 +2,20 @@
 	<h2><?php _e( 'Image Repair', 'pig' ); ?></h2>
 	<?php if( $images->have_posts() ): ?>
 		<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
-			<table class="wide-fat">
+			<table class="widefat">
 				<thead>
 					<tr>
+						<th>Post ID</th>
 						<th>Post Title</th>
-						<th>Expiration Date</th>
-						<th colspan="2">Actions</th>
+						<th>Purge Date</th>
+						<th colspan="2" class="actions">Actions</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php while( $images->have_posts() ): $images->the_post();
 						$expires = get_post_meta( get_the_ID(), '_pig_image_404', true ); ?>
 						<tr>
+							<td><?php the_ID(); ?>
 							<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
 							<td><?php echo date( 'n/j/Y g:i a', $expires ); ?></td>
 							<td><a href="<?php echo get_edit_post_link( get_the_ID() ); ?>">Edit</a></td>
