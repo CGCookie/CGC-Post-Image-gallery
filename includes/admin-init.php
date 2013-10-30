793b64826830a8f22cc4e8189213b72c874c972e
Brian DiChaira <briandichiara@gmail.com>
2013-10-30 00:20:35 -0500

    formatted admin table

--- includes/admin-init.php
+++ includes/admin-init.php
@@ -8,9 +8,14 @@ function pig_admin_menu(){
 
 function pig_admin_image_repair(){
 
+	$limit = 50;
+	$current_page = isset( $_GET['paged'] ) ? (int)sanitize_text_field( $_GET['paged'] ) : 1;
+	$offset = $current_page == 1 ? 0 : $limit * ( $current_page - 1 );
+
 	$images = new WP_Query( array(
 		'post_type' => 'images',
-		'posts_per_page' => '50',
+		'posts_per_page' => $limit,
+		'offset' => $offset,
 		'post_status' => 'publish',
 		'meta_query' => array(
 			array(
@@ -20,6 +25,26 @@ function pig_admin_image_repair(){
 			)
 		)
 	) );
+
+	$admin_url = admin_url( 'upload.php?page=pig-image-repair');
+
+	$pages = ceil( $images->found_posts / $limit );
+	$first = $admin_url;
+	$last = add_query_arg( array( 'paged' => $pages ), $admin_url );
+	$prev = $current_page <= 2 ? $first : add_query_arg( array( 'paged' => $current_page - 1 ), $admin_url );
+	$next = $current_page >= $pages - 1 ? $last : add_query_arg( array( 'paged' => $current_page + 1 ), $admin_url );
+
+	$pagination = '<div class="tablenav-pages">';
+		$pagination .= '<span class="displaying-num">' . $images->post_count . ' items</span>';
+		$pagination .= '<span class="pagination-links">';
+			$pagination .= '<a class="first-page' . ($current_page == 1 ? ' disabled' : '') . '" title="Go to the first page" href="' . $first . '">&laquo;</a>';
+			$pagination .= '<a class="prev-page' . ($current_page == 1 ? ' disabled' : '') . '" title="Go to the previous page" href="' . $prev . '">&lsaquo;</a>';
+			$pagination .= ' <span class="paging-input">' . $current_page . ' of <span class="total-pages">' . $pages . '</span></span> ';
+			$pagination .= '<a class="next-page' . ($current_page == $pages ? ' disabled' : '') . '" title="Go to the next page" href="' . $next . '">&rsaquo;</a>';
+			$pagination .= '<a class="last-page' . ($current_page == $pages ? ' disabled' : '') . '" title="Go to the last page" href="' . $last . '">&raquo;</a>';
+		$pagination .= '</span>';
+	$pagination .= '</div>';
+	$pagination .= '<br class="clear">';
 
 	include( 'admin-page.php' );
 }
--- includes/admin-page.php
+++ includes/admin-page.php
@@ -2,6 +2,7 @@
 	<h2><?php _e( 'Image Repair', 'pig' ); ?></h2>
 	<?php if( $images->have_posts() ): ?>
 		<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+			<div class="tablenav top"><?php echo $pagination; ?></div>
 			<table class="widefat">
 				<thead>
 					<tr>
@@ -12,18 +13,21 @@
 					</tr>
 				</thead>
 				<tbody>
-					<?php while( $images->have_posts() ): $images->the_post();
+					<?php $alternate = true;
+					while( $images->have_posts() ): $images->the_post();
 						$expires = get_post_meta( get_the_ID(), '_pig_image_404', true ); ?>
-						<tr>
+						<tr<?php if( $alternate ) echo ' class="alternate"'; ?>>
 							<td><?php the_ID(); ?>
 							<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
 							<td><?php echo date( 'n/j/Y g:i a', $expires ); ?></td>
 							<td><a href="<?php echo get_edit_post_link( get_the_ID() ); ?>">Edit</a></td>
 							<td><a href="<?php echo get_delete_post_link( get_the_ID() ); ?>">Delete</a></td>
 						</tr>
-					<?php endwhile; wp_reset_postdata(); ?>
+					<?php $alternate = !$alternate;
+					endwhile; wp_reset_postdata(); ?>
 				</tbody>
 			</table>
+			<div class="tablenav bottom"><?php echo $pagination; ?></div>
 		</form>
 	<?php else: ?>
 		<p><?php _e( 'No images in need of repair.', 'pig' ); ?>
